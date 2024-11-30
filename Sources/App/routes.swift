8d1b6c049e6b4c3243b4d96f69a0cf91e5a81ebf
Darserg <Darserg1212>
2024-11-30 11:57:18 +0400

    Add new routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,11 +3,29 @@ import Vapor
 
 func routes(_ app: Application) throws {
     let userController = UserController()
+    let preferenceContoller = PreferenceController()
+    let questionConroller = QuestionController()
     
+    // users
     app.get("users", use: userController.getAll)
+    app.get("users", ":userID", use: userController.getOne)
     app.post("users", use: userController.create)
     app.put("users", ":userID", use: userController.update)
     app.delete("users", ":userID", use: userController.delete)
-    app.get("users", ":userID", use: userController.getOne)
+    
+    // questions
+    app.get("questions", use: questionConroller.getAll)
+    app.get("questions", ":questionID", use: questionConroller.getOne)
+    app.post("questions", use: questionConroller.create)
+    app.put("questions", ":questionID", use: questionConroller.update)
+    app.delete("questions", ":questionID", use: questionConroller.delete)
+    
+    // preference
+    app.get("preferences", use: preferenceContoller.getAll)
+    app.get("preferences", ":prefID", use: preferenceContoller.getOne)
+    app.post("preferences", use: preferenceContoller.create)
+    app.put("preferences", ":prefID", use: preferenceContoller.update)
+    app.delete("preferences", ":prefID", use: preferenceContoller.delete)
+    
 }
  
